07f3bfa92bfd9b59d38957e1f163dfbfe99cdfbf
System-of-Root <root@r00t.systems>
2024-02-25 02:38:16 -0500

    initial setup

---
+++ RoundsModdingTools/Main.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnityEngine;
+
+namespace ModdingTools {
+    [BepInDependency("com.willis.rounds.unbound")] 
+    [BepInPlugin("TODO", "ModdingTools", "1.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class Main : BaseUnityPlugin {
+        
+        void awake() {
+
+        }
+
+        void Start() {
+
+        }
+    }
+}
