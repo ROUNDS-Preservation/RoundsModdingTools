05a761734e2a518c07a57621f7fe615d85bbbb4c
System-of-Root <root@r00t.systems>
2024-02-27 18:20:26 -0500

    <@429810730691461130> Please FECKING use the style the project is coded in
    For the love of fecking gods, I am getting tired of reformatting all of your fecking code

--- RoundsModdingTools/Patches/HealthHandlerPatch.cs
+++ RoundsModdingTools/Patches/HealthHandlerPatch.cs
@@ -2,32 +2,27 @@
 using ModdingTools.Handler;
 using UnityEngine;
 
-namespace ModdingTools.Patches
-{
+namespace ModdingTools.Patches {
     [HarmonyPatch(typeof(HealthHandler))]
-    internal class HealthHandlerPatch
-    {
+    internal class HealthHandlerPatch {
         [HarmonyPatch("DoDamage")]
         [HarmonyPrefix]
-        public static void DoDamage(HealthHandler __instance, ref Vector2 damage, Vector2 position, Color blinkColor, GameObject damagingWeapon, Player damagingPlayer, bool healthRemoval, bool lethal, bool ignoreBlock)
-        {
+        public static void DoDamage(HealthHandler __instance, ref Vector2 damage, Vector2 position, Color blinkColor, GameObject damagingWeapon, Player damagingPlayer, bool healthRemoval, bool lethal, bool ignoreBlock) {
             CharacterData data = (CharacterData)Traverse.Create(__instance).Field("data").GetValue();
             DeathHandler.PlayerTakeDamage(data.player, damagingPlayer);
         }
 
         [HarmonyPatch("RPCA_Die")]
         [HarmonyPrefix]
-        public static void RPCA_Die(Player ___player, Vector2 deathDirection)
-        {
-            if (___player.data.dead) return;
+        public static void RPCA_Die(Player ___player, Vector2 deathDirection) {
+            if(___player.data.dead) return;
             DeathHandler.PlayerDied(___player);
         }
 
         [HarmonyPatch("RPCA_Die_Phoenix")]
         [HarmonyPrefix]
-        public static void RPCA_Die_Phoenix(HealthHandler __instance, Player ___player, Vector2 deathDirection)
-        {
-            if ((___player.data.dead || __instance.isRespawning)) return;
+        public static void RPCA_Die_Phoenix(HealthHandler __instance, Player ___player, Vector2 deathDirection) {
+            if((___player.data.dead || __instance.isRespawning)) return;
             DeathHandler.PlayerDied(___player);
         }
     }
